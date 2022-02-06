a15b074df6cb95b5a0eecce914aa93baca7c0d57
catherine.michiko.webster <catherine.michiko.webster@linecorp.com>
2022-02-06 13:20:35 +0900

    clean up

--- src/test/java/com/webster/forexproxy/WireMockInitializer.java
+++ src/test/java/com/webster/forexproxy/WireMockInitializer.java
@@ -1,7 +1,13 @@
 package com.webster.forexproxy;
 
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
+
+import java.util.Map;
+
 import com.github.tomakehurst.wiremock.WireMockServer;
 import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
+
+import org.springframework.boot.test.util.TestPropertyValues;
 import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.event.ContextClosedEvent;
@@ -15,7 +21,7 @@ public class WireMockInitializer implements ApplicationContextInitializer<Config
     public void initialize(ConfigurableApplicationContext applicationContext) {
 
         WireMockServer wireMockServer =
-                new WireMockServer(new WireMockConfiguration().port(9000));
+                new WireMockServer(new WireMockConfiguration().dynamicPort());
 
         wireMockServer.start();
 
@@ -24,6 +30,10 @@ public class WireMockInitializer implements ApplicationContextInitializer<Config
                 wireMockServer.stop();
             }
         });
+
+        TestPropertyValues
+                .of(Map.of("one-frame.base-uri", "http://localhost:" + wireMockServer.port() + "/rates"))
+                .applyTo(applicationContext);
 
         applicationContext.getBeanFactory()
                           .registerSingleton("wireMockServer", wireMockServer);
